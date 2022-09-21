ce48a788af66f061cfd85a5870efd101799a6d41
Ali <alifeyza4017@gmail.com>
2022-09-21 12:21:06 +0300

    added

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 var items = ["item 1","item 2", "item 3", "item 4"]
 
 
-var list = document.querySelector("#myList")
-var html = ""
+let list = document.querySelector("#myList")
+let input = document.querySelector("#txtItem")
 
-items.forEach((item)=> {
-  html += `<li class="list-group-item">${item}</li>`;
+// ** Alternative working
+// var html = ""
+
+// items.forEach((item)=> {
+//   html += `<li class="list-group-item">${item}</li>`;
+// })
+
+// list.innerHTML = html
+
+items.forEach(function(item) {
+  
+  let li = document.createElement("li")
+  li.setAttribute("class","list-group-item")
+  
+  let text = document.createTextNode(item)
+  li.append(text)
+  let span = document.createElement("span")
+  span.className = "close"
+  let content = document.createTextNode("X")
+  span.appendChild(content)
+  li.append(span)
+  list.appendChild(li)
+
+
 })
-
-list.innerHTML = html
