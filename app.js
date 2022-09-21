059954a418b3730ed0d0c56c9ac085a638ad9e5a
Ali <alifeyza4017@gmail.com>
2022-09-21 13:04:37 +0300

    added

--- app.js
+++ app.js
@@ -15,19 +15,17 @@ let input = document.querySelector("#txtItem")
 
 
 items.forEach(function(item) {
-  
+  let li = document.createElement("li");
+  li.setAttribute("class", "list-group-item");
 
-  let li = document.createElement("li")
-  li.setAttribute("class","list-group-item")
-  
-  let text = document.createTextNode(item)
-  li.append(text)
-  let span = document.createElement("span")
-  span.className = "close"
-  let content = document.createTextNode("X")
-  span.appendChild(content)
-  li.append(span)
-  list.appendChild(li)
+  let text = document.createTextNode(item);
+  li.append(text);
+  let span = document.createElement("span");
+  span.className = "close";
+  let content = document.createTextNode("X");
+  span.appendChild(content);
+  li.append(span);
+  list.appendChild(li);
 
 })
 
@@ -35,4 +33,17 @@ list.addEventListener("click", function(e) {
   if(e.target.tagName = "li") {
     e.target.classList.toggle("checked")
   }
+
 })
+
+close = document.querySelectorAll(".close")
+
+close.forEach((i)=> {
+       i.addEventListener("click", (a)=> {
+        if(a.target.classList.contains("close")) {
+          i.parentElement.remove()
+        }
+       })
+})
+
+
